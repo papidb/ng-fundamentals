c0ca6ad0db1821ea70f39424e4dc16cc96c5e1b0
Benjamin Daniel <benjamindaniel706@gmail.com>
2019-10-27 09:26:25 +0100

    added events-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent
+  ],
+  // providers: [],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,37 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "events-list",
+  template: `
+    <div>
+      <h1>Upcoming Angular Events</h1>
+      <h2>{{ event.name }}</h2>
+      <hr />
+      <div class="well hoverwell thumbnail">
+        <div>Date: {{ event.date }}</div>
+        <div>Time: {{ event.time }}</div>
+        <div>Price: \${{ event.price }}</div>
+        <div>
+            <span>Location: {{event.location.address}}</span>
+            <span>&nbsp;</span>
+            <span>{{event.location.city}}</span>
+        </div>
+      </div>
+    </div>
+  `
+})
+export class EventsListComponent {
+  event = {
+    id: 1,
+    name: "Angular Connect",
+    date: "9/26/2036",
+    time: "10:00 am",
+    price: 400,
+    imageUrl: "/assets/images/angularconnect-shield.png",
+    location: {
+      address: "1084 sdk",
+      city: "Lagos",
+      country: "Putdi"
+    }
+  };
+}
